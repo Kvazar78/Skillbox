30b4638a471f60614d2c4ab8481b99fee4b32294
Kvazar78 <kvazar78@mail.ru>
2020-11-17 22:02:28 +0700

    поправлено 7_10

--- 7_for/dz/task_10.py
+++ 7_for/dz/task_10.py
@@ -4,7 +4,9 @@
 start_segment = int(input('Введите начало отрезка: '))
 end_segment = int(input('Введите конец отрезка: '))
 average = 0
+counter = 0
 for dot in range(start_segment, end_segment + 1):
     if dot % 3 == 0:
         average += dot
-print(f'среднее арифметическое всех чисел из отрезка [a; b], которые кратны числу 3 = {average}')
+        counter += 1
+print(f'среднее арифметическое всех чисел из отрезка [a; b], которые кратны числу 3 = {average / counter}')
