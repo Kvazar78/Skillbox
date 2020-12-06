96f1257f611671ebf8b6f7157803de0e21914d07
Kvazar78 <kvazar78@mail.ru>
2020-12-06 12:13:40 +0700

    11 раздел - ДЗ 11-15

--- 11_float_round/dz/task_11.py
+++ 11_float_round/dz/task_11.py
@@ -14,9 +14,9 @@
 #
 # Конь в клетке (0, 1). Точка в клетке (2, 0).
 # Да, конь может ходить в эту точку.
-flag = False
+flag_coordinates = False
 
-while flag ==False:
+while flag_coordinates ==False:
     print('Введите местоположение коня')
     x = float(input(': '))
     y = float(input(': '))
@@ -24,7 +24,7 @@ while flag ==False:
     x_dot = float(input(': '))
     y_dot = float(input(': '))
     if 0 < x < 0.8 and 0 < y < 0.8 and 0 < x_dot < 0.8 and 0 < y_dot < 0.8:
-        flag = True
+        flag_coordinates = True
     else:
         print('Придется вводить все заново - где-то опечата!')
 
@@ -34,12 +34,22 @@ x_dot = int((x_dot * 10) % 10)
 y_dot = int((y_dot * 10) % 10)
 
 print(f'Конь в клетке ({x}, {y}). Точка в клетке ({x_dot}, {y_dot}).')
-flag_move = False
+flag_Xmove = False
+flag_Ymove = False
 
-if ((x + 2) or (x - 2) or (x + 1) or (x - 1)) == x_dot and ((y - 1) or (y + 1)) == y_dot:
-    flag_move = True
+list_x = [x + 2, x - 2, x + 1, x - 1]
+list_y = [y + 2, y - 2, y + 1, y - 1]
 
-if flag_move:
+for i in list_x:
+    if i == x_dot:
+        flag_Xmove = True
+        break
+for i in list_y:
+    if i == y_dot:
+        flag_Ymove = True
+        break
+
+if flag_Xmove and flag_Ymove:
     print('Да, конь может ходить в эту точку.')
 else:
     print('Сюда ходить нельзя')
