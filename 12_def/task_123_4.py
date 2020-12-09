4e89c6a3296bcc34380a9678621961c85ab451be
Kvazar78 <kvazar78@mail.ru>
2020-12-09 22:55:15 +0700

    12 раздел - 12.3 готово

--- 12_def/task_123_4.py
+++ 12_def/task_123_4.py
@@ -4,3 +4,24 @@
 # которая проверяет сколько из этих чисел являются простыми. Для проверки простоты числа
 # реализуйте функцию isPrime
 def isPrime(x):
+    if x % 2 == 0 and x != 2:
+        noProst = True
+    else:
+        for i in range(3, x // 2):
+            if x % i == 0:
+                noProst = True
+                break
+            else:
+                noProst = False
+    if noProst:
+        print('Число не простое')
+    else:
+        print('Число простое')
+
+
+num = int(input('Сколько будет чисел? '))
+
+while num > 0:
+    number = int(input('Введи число: '))
+    isPrime(number)
+    num -= 1
