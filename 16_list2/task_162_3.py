9e42dc161c947d510108dc6bda0b65fab839d2b9
Kvazar78 <kvazar78@mail.ru>
2021-01-19 21:32:48 +0700

    16.3 раздел - готово

--- 16_list2/task_162_3.py
+++ 16_list2/task_162_3.py
@@ -1,3 +1,18 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',  'Леон', 'Богемская рапсодия', 'Город грехов',  'Мементо', 'Отступники', 'Деревня',  'Проклятый остров', 'Начало', 'Матрица']
 list_favorites = []
 
+while True:
+    print('\nВаш текущий топ фильмов: ', list_favorites)
+    my_film = input('Введи название фильма: ')
+    if my_film in list_favorites:
+        print('Такой фильм в коллекции уже есть! Выбери другой...')
+    else:
+        select = input('Выбери действие (добавить, удалить, вставить): ')
+        if select == 'добавить':
+            list_favorites.append(my_film)
+        elif select == 'удалить':
+            list_favorites.remove(my_film)
+        elif select == 'вставить':
+            print(list_favorites)
+            place = int(input('Выбери позицию куда добавить фильм: '))
+            list_favorites.insert(place - 1, my_film)
