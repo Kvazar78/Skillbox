1759664e4665eed816ea5277567425a2262df084
Kvazar78 <kvazar78@mail.ru>
2021-01-20 08:32:05 +0700

    16.3 раздел - готово

--- 16_list2/task_163_3.py
+++ 16_list2/task_163_3.py
@@ -1,22 +1,23 @@
 amount_pkt = int(input('Кол-во пакетов: '))
 list_in = []
 list_out = []
-err_count = 0
+# err_count = 0
 lost_pkt = 0
 
 for i_pkt in range(amount_pkt):
-    print('Пакет номер', i_pkt + 1)
+    print('\nПакет номер', i_pkt + 1)
     for i_elm in range(4):
         elem = int(input(f'{i_elm + 1} бит: '))
         list_in.append(elem)
     if list_in.count(-1) <= 1:
-        err_count += 1
         list_out.extend(list_in)
+        # if list_in.count(-1) == 1:
+        #     err_count += 1
     else:
         print('Много ошибок в пакете.')
         lost_pkt += 1
     list_in = []
 
 print('\nПолученное сообщение:', list_out)
-print('Кол-во ошибок в сообщении:', err_count)
+print('Кол-во ошибок в сообщении:', list_out.count(-1))
 print('Кол-во потерянных пакетов:', lost_pkt)
