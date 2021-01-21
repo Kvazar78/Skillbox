30286ba83b778d8c934f505c7b5c434b4c6799c8
Kvazar78 <kvazar78@mail.ru>
2021-01-21 16:51:50 +0700

    16.4 раздел - готово

--- 16_list2/task_164_3.py
+++ 16_list2/task_164_3.py
@@ -3,8 +3,9 @@ fruit_name = input('Новый фрукт: ')
 price = int(input('Цена: '))
 
 goods.append([fruit_name, price])
-print('Новый ассортимент:', goods)
+print('\nНовый ассортимент:', goods)
 
-print(goods[1][1])
+for i in goods:
+    i[1] = round(i[1] * 1.08, 1)
 
-# for i in goods:
+print('\nНовый ассортимент с увел. ценой:', goods)
