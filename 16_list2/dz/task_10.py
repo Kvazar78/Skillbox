69a3060a353760abd754397d2cb328b3d72b0fc8
Kvazar78 <kvazar78@mail.ru>
2021-01-24 22:20:58 +0700

    16 ДЗ - готово

---
+++ 16_list2/dz/task_10.py
@@ -0,0 +1,28 @@
+count_num = int(input('Кол-во чисел: '))
+sequence = []
+sequence_reverse = []
+
+for _ in range(count_num):
+    sequence.append(int(input('Число: ')))
+
+for i in range(len(sequence)- 1, -1, -1):
+    sequence_reverse.append(sequence[i])
+
+if len(sequence) > 1:
+    for i in range(len(sequence)- 1, -1, -1):
+        if i == 0 or len(sequence) == 1:
+            break
+        elif sequence[i] != sequence[i - 1]:
+            sequence_reverse.remove(sequence_reverse[0])
+            break
+        elif sequence[i] == sequence_reverse[0]:
+            sequence_reverse.remove(sequence_reverse[0])
+else:
+
+print('\nПоследовательность:', end=' ')
+for i in sequence:
+    print(i, end=' ')
+print('\nНужно приписать чисел:',len(sequence_reverse))
+print('Сами числа:', end=' ')
+for i in sequence_reverse:
+    print(i, end=' ')
--- 16_list2/dz/task_6.py
+++ 16_list2/dz/task_6.py
@@ -11,3 +11,12 @@ list_second = input_list(7, 'второго списка')
 
 print('Первый список:', list_first)
 print('Второй список:', list_second)
+
+list_first += list_second
+
+for i_num in list_first:
+    if list_first.count(i_num) > 1:
+        for _ in range(list_first.count(i_num) - 1):
+            list_first.remove(i_num)
+
+print('\nНовый первый список с уникальными элементами:', list_first)
