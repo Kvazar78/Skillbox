857bf3f73d82498a58e9181e42fb7db2cf6d56cd
Kvazar78 <kvazar78@mail.ru>
2021-01-26 01:02:59 +0700

    16 ДЗ - 10 исправлено

--- 16_list2/dz/task_10.py
+++ 16_list2/dz/task_10.py
@@ -1,6 +1,14 @@
-def print_sequence(seq):
-    for i in seq:
-        print(i, end=' ')
+def comparison(seq, seq_r, i_s):
+    result = False
+    i_sr = 0
+    for num in range(i_s, len(sequence)):
+        if seq[num] ==seq_r[i_sr]:
+            i_sr += 1
+            result = True
+        else:
+            result = False
+            break
+    return result
 
 
 count_num = int(input('Кол-во чисел: '))
@@ -14,23 +22,21 @@ for i in range(len(sequence)- 1, -1, -1):
     sequence_reverse.append(sequence[i])
 
 print('\nПоследовательность:', end=' ')
-print_sequence(sequence)
+for i in sequence:
+    print(i, end=' ')
 print()
 
 if len(sequence) == 1:
     print('\nОдно число?')
-elif sequence == sequence_reverse:
-    print('\nСписок уже зеркальный, добавлять ни чего не надо')
 else:
-    for i in range(len(sequence)- 1, -1, -1):
-        if i == 0:
+    iter = 0
+    for i in range(len(sequence)):
+        if comparison(sequence, sequence_reverse, i):
             break
-        elif sequence[i] != sequence[i - 1]:
-            sequence_reverse.remove(sequence_reverse[0])
-            break
-        elif sequence[i] == sequence_reverse[0]:
-            sequence_reverse.remove(sequence_reverse[0])
-            # sequence.remove(sequence[-1])
-    print('\nНужно приписать чисел:', len(sequence_reverse))
-    print('Сами числа:', end=' ')
-    print_sequence(sequence_reverse)
+        else:
+            iter += 1
+
+print('\nНужно приписать чисел:', iter)
+print('Сами числа:', end=' ')
+for i in range((iter* (-1)), 0):
+    print(sequence_reverse[i], end=' ')
