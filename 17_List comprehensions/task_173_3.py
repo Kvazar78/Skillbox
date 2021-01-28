4c36c294398a3fa9682da956890931b1010abe80
Kvazar78 <kvazar78@mail.ru>
2021-01-28 14:48:04 +0700

    17 раздел - ДЗ начало

--- 17_List comprehensions/task_173_3.py
+++ 17_List comprehensions/task_173_3.py
@@ -22,3 +22,6 @@ import random
 
 squad_1 = [random.randint(50, 80) for _ in range(10)]
 squad_2 = [random.randint(30, 60) for _ in range(10)]
+
+squad_3 = ['Погиб' if squad_1[i] + squad_2[i] > 100 else 'Выжил' for i in range(10)]
+print('Состояние третьего отряда:', squad_3)
