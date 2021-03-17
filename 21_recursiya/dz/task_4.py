40a9283075a5a59f2bc141a373d6166be756cfcf
Kvazar78 <kvazar78@mail.ru>
2021-03-17 16:39:55 +0700

    21 модуль - дз 5

--- 21_recursiya/dz/task_4.py
+++ 21_recursiya/dz/task_4.py
@@ -4,8 +4,6 @@
 # до которого будет просматриваться структура. Напишите функцию, которая находит заданный пользователем ключ
 # в словаре и выдаёт значение этого ключа на экран. По умолчанию уровень не задан. В качестве примера можно
 # использовать такой словарь:
-
-
 site = {
     'html': {
         'head': {
@@ -14,8 +12,43 @@ site = {
         'body': {
             'h2': 'Здесь будет мой заголовок',
             'div': 'Тут, наверное, какой-то блок',
-            'p': 'А вот здесь новый абзац'
+            'p': 'А вот здесь новый абзац',
+            'footer': {
+                'h6': 'как меня замучила рекурсия',
+                'div': 'снова какой-то блок, теперь в футере и найти его не получается, т.к. выше видимо уже есть',
+            }
         }
     }
 }
 
+
+def find_key(site, key, depth=0, depth_flag=False, step=0):
+    if key in site:
+        return site[key]
+
+    for sub_key in site.values():
+        if step == depth and depth_flag:
+            result = None
+            break
+        if isinstance(sub_key, dict):
+            result = find_key(sub_key, key, depth, depth_flag, step + 1)
+            if result:
+                break
+    else:
+        result = None
+    return result
+
+key = input('Какой ключ ищем? ')
+select = input('Будем указывать глубину поиска?(да/нет) ')
+
+if select == 'да':
+    depth = int(input('Глубина поиска (0 - начальный уровень): '))
+    depth_flag = True
+    value = find_key(site, key, depth, depth_flag)
+else:
+    value = find_key(site, key)
+
+if value:
+    print('Значение: ', value)
+else:
+    print('Такого ключа нет')
