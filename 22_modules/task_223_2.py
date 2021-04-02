8b635c5cfedb1ee34a611d53d989f78511db2d25
Kvazar78 <kvazar78@mail.ru>
2021-04-02 15:30:07 +0700

    22 модуль - 22.3.2

--- 22_modules/task_223_2.py
+++ 22_modules/task_223_2.py
@@ -8,3 +8,25 @@
 # (выбор можно сгенерировать случайно).
 #
 # Подсказка: можно использовать, например, список для сохранения найденного пути.
+import os
+
+
+def find_file(path_to_file, file_name, lst_path=None):
+    lst_path = lst_path if lst_path else []
+    for i_elem in os.listdir(path_to_file):
+        path = os.path.join(path_to_file, i_elem)
+        if file_name == i_elem:
+            print('Файл найден в:', path)
+            # lst_path.append(path)
+        if os.path.isdir(path):
+            find_file(path, file_name, lst_path)
+            lst_path.append(path)
+    # return lst_path
+
+
+path_to_file = input('Ищем в: ')
+file_name = input('Имя файла: ')
+print()
+
+list_files = find_file(path_to_file, file_name)
+print(list_files)
