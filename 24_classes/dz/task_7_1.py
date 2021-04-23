91245f2e66f3a2c6801eaf54c8a5d3a09efd2559
Kvazar78 <kvazar78@mail.ru>
2021-04-23 15:21:31 +0700

    24 модуль - ДЗ 2.3 часть ООП

--- 24_classes/dz/task_7_1.py
+++ 24_classes/dz/task_7_1.py
@@ -4,29 +4,38 @@ from random import randint
 class Human:
     corpse_flag = False
 
-    def __init__(self, name):
+    def __init__(self, name, house):
         self.name = name
         self.satiety = 50
-        self.house = House()
+        self.house = house
+
+    def act(self):
+        fortuna = randint(1, 4)
+        if fortuna == 1:
+            self.eat()
+        elif fortuna == 2:
+            self.go_to_work()
+        elif fortuna == 3:
+            self.shopping()
+        elif fortuna == 4:
+            self.play()
 
     def eat(self):
-        if self.house.food > 10:
+        if self.satiety < 20 and self.house.food > 10:
             self.house.food -= 10
             self.satiety += 10
             print('Хорошо покушал...!')
         else:
-            print(f'Так как сейчас в доме {self.house.food}, то жрать особо не чего - пошел в магаз...')
-            self.shopping()
+            print(f'Так как сейчас в доме {self.house.food}, то жрать особо не чего...')
         self._status_house()
 
     def go_to_work(self):
-        if self.satiety >= 15:
+        if self.house.money < 50 and self.satiety >= 15:
             self.satiety -= 15
             self.house.money += 20
-            print(f'{self.name} поработал, заработал но и проголодался...', self._status_house())
+            print(f'{self.name} поработал, заработал но и проголодался...\n', self._status_house())
         else:
             print(self._info_hunger(), 'топать на работу, надо поесть!')
-            self.eat()
 
     def shopping(self):
         if self.house.money > 10:
@@ -35,14 +44,12 @@ class Human:
             print('Продукты куплены, деньги потрачены', self._status_house())
         else:
             print(f'У {self.name} мало денег! Надо поработать!')
-            self.go_to_work()
 
     def play(self):
         if self.satiety > 10:
             self.satiety -= 10
         else:
             print(self._info_hunger(), 'играть, надо поесть!')
-            self.eat()
 
     def _status_house(self):
         return f'Сейчас в доме {self.house.food} еды и {self.house.money} денег'
@@ -58,7 +65,15 @@ class House:
         self.money = 0
 
 
-person = Human('Kolya')
+crazy_house = House()
+person = Human('Kolya', crazy_house)
+count_days = 0
 
-while person.satiety > 0:
-    fortune = randint(1, 6)
+for _ in range(365):
+    if person.satiety <= 0:
+        print(f'{person.name} - сдох!')
+        break
+    else:
+        count_days += 1
+        print(f'День - {count_days}:')
+        person.act()
